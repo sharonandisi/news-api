284194e01cfb4ac06bacbc06bf510b33d96e8664
sharonandisi <sharonandisi.28@gmail.com>
2018-11-08 11:54:52 +0300

    import devconfig subclass in __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 #Initializing the application
 app = Flask(__name__)
+
+#setting up configuration
+app.config.from_objects(DevConfig)
 
 from app import views
