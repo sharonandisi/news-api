c0a48bc028e7ac40ad563465d7294517d0d593c6
sharonandisi <sharonandisi.28@gmail.com>
2018-11-08 11:24:30 +0300

    add a new newssource route

--- app/views.py
+++ app/views.py
@@ -7,4 +7,13 @@ def index():
     """
     View root page function that returns the index page and its data
     """
+
+    message = 'News API'
     return render_template('index.html')
+
+@app.route('/newssource/<int :source_id>')
+def newssource(source_id):
+    """
+    View newssource page function that returns news source page and its data
+    """
+    return render_template('newssource.html', id = source_id)
