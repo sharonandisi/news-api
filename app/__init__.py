aeda8b4503634068d16258643418f8f5e01b1323
sharonandisi <sharonandisi.28@gmail.com>
2018-11-08 12:16:03 +0300

    we link the application to the new configuration file

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from .config import DevConfig
 
 #Initializing the application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config= True)
 
 #setting up configuration
 app.config.from_objects(DevConfig)
+app.config.from_pylife('config.py')
 
 from app import views
