2f6c99df6ad707714b14d16876df022039d1df81
sharonandisi <sharonandisi.28@gmail.com>
2018-11-09 12:02:34 +0300

    create unittest for testing behaviour of the news source class

---
+++ app/models/newssource.py
@@ -0,0 +1,9 @@
+class Newssource:
+    '''
+    Newssource class to define Newssource Objects
+    '''
+
+    def __init__ (self,id, name, urlToImage):
+        self.id = id
+        self.name = name
+        self.urlToImage = urlToImage
