1b0a93ead23b3d18b5908ac2115db389a9497bb2
sharonandisi <sharonandisi.28@gmail.com>
2018-11-11 17:15:16 +0300

    create process function results

--- app/request.py
+++ app/request.py
@@ -9,21 +9,43 @@ api_key = app.config['NEWS_API_KEY']
 #Getting the news base url
 base_url = app.config["NEWS_API_BASE_URL"]
 
-def get_source(category):
+def get_sources(category):
     '''
     Function that gets the json response to the url request
     '''
-    get_source_url = base_url.format(category,api_key)
+    get_sources_url = base_url.format(category,api_key)
 
-    with urllib.request.urlopen(get_source_url) as url:
-        get_source_data = url.read()
-        get_source_response = json.loads(get_source_data)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
 
         source_results = None
 
-        if get_source_response['results']:
-            source_results_list = get_source_response['results']
+        if get_sources_response['results']:
+            source_results_list = get_sources_response['results']
             source_results = process_results(source_results_list)
 
     return source_results
 
+    def process_results(source_list):
+        """
+        Function that processes the source result and transform them to a list of Objects
+
+        Args:
+            source_list: A list of dictionaries that contain source details
+
+        Returns :
+            source_results: A list of source objects
+        """
+        source_results = []
+        for source_item in source_list:
+            id = source_item.get('id')
+            name = source_item.get('name')
+            urlToImage = source_item.get('urlToImage_path')
+            publishedAt = source_item.get('publishedAt')
+            url = source_item.get ('url')
+
+            if poster:
+                source_object = Source(id,name,urlToImage,publishedAt,url)
+                source_results.append(source.object)
+    return source_results
